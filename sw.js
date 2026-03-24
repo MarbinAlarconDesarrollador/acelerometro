3bb7e7b86cd656354467c2da94cccbd2bd6cd1de
Marbin Alarcon <marbinalarcon9@gmail.com>
2026-03-24 12:48:35 -0500

    manifest

--- sw.js
+++ sw.js
@@ -1,17 +1,24 @@
-const CACHE_NAME = 'pausa-v1';
+const CACHE_NAME = 'space-stretch-v1';
 const ASSETS = [
-  'index.html',
-  'manifest.json'
+  './',
+  './index.html',
+  './manifest.json'
 ];
 
+// Instalar y guardar archivos en caché
 self.addEventListener('install', (e) => {
   e.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
   );
 });
 
+// Responder desde el caché cuando no hay red
 self.addEventListener('fetch', (e) => {
   e.respondWith(
-    caches.match(e.request).then((res) => res || fetch(e.request))
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
   );
 });
